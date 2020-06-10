f90d0fc09c7b0cea451f0dd7460fb4c17b7ba7bc
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-10 09:42:41 -0600

    Test market exists

--- test/market_test.rb
+++ test/market_test.rb
@@ -6,9 +6,10 @@ require './lib/market'
 
 class MarketTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_instance_of Market, market
+  end
 
   #def test_it_has_attributes
   #end
