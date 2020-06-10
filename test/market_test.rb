be889e299ea8125bc0dac6ab92192a491bdc5130
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-10 09:41:33 -0600

    Create Market class and MarketTest

---
+++ test/market_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
