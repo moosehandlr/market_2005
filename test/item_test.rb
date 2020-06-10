7204afa09a74b6b8f8e0ccf6bcc8775dd3f591c2
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-10 09:04:26 -0600

    Test Item exists

--- test/item_test.rb
+++ test/item_test.rb
@@ -4,9 +4,10 @@ require './lib/item'
 
 class ItemTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    assert_instance_of Item, item1
+  end
 
   #def test_it_has_attributes
   #end
