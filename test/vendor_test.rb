d349dfc49738ad9bbcf7f52ce2d188e3d5c479cc
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-10 10:32:02 -0600

    Test vendor can find potential revenue

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -44,6 +44,26 @@ class VendorTest < Minitest::Test
     assert_equal 55, vendor.check_stock(item1)
   end
 
+  def test_vendor_can_calculate_potential_revenue
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+
+    assert_equal 29.75, vendor1.potential_revenue
+  end
+
 
 
 end
