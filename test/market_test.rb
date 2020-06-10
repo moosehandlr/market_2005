f2872dea6541ad154fc6bb1c299f24924a8d1852
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-10 09:44:49 -0600

    Test market has vendors

--- test/market_test.rb
+++ test/market_test.rb
@@ -14,6 +14,7 @@ class MarketTest < Minitest::Test
   def test_it_has_attributes
     market = Market.new("South Pearl Street Farmers Market")
     assert_equal "South Pearl Street Farmers Market",  market.name
+    assert_equal [], market.vendors
   end
 
 end
