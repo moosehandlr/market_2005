d91d5e41aeaf300dcb253de1e5d8c36937533eca
Alex Desjardins <ardesjardins91@gmail.com>
2020-06-10 09:26:43 -0600

    Update test vendor can check stock by item

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -21,7 +21,9 @@ class VendorTest < Minitest::Test
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
 
-    assert_equal 0, vendor.check_stock(item1)
+    vendor.stock(item1, 30)
+
+    assert_equal 30, vendor.check_stock(item1)
   end
 
   def test_vendor_can_stock_item
